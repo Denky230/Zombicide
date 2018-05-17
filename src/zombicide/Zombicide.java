3a36a62cecad7c0e5a085149bc1db7d370cc4d39
Denky <sorek230@gmail.com>
2018-05-17 02:00:11 +0200

    Main 	- Added game loop 	- Minor code restructure

--- src/zombicide/Zombicide.java
+++ src/zombicide/Zombicide.java
@@ -7,13 +7,18 @@ import weapons.*;
 import zombies.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Zombicide {
 
     static Survivor[] myTeam;
 
     public static void main(String[] args) {
+        int NUM_FLOORS = 3;
+        int currFloor = 1;
+        
+        int HORDE_SIZE = 8;
+        List<Zombie> zombieHorde = new ArrayList<>();
+        
         // Set survivors
         myTeam = new Survivor[] {
             new Survivor("Rick", "COWABUNGA!", Skills_enum.FAST, new Katana()),
@@ -21,16 +26,29 @@ public class Zombicide {
             new Survivor("John", "COME AT ME!", Skills_enum.STRONG, new Shotgun()),
             new Survivor("Manueh"),
             new Survivor("Carla")
-        };
-        
-        // Set zombies
-        List<Zombie> zombieHorde = new ArrayList<>();
-        for (int i = 0; i < 8; i++){
-            zombieHorde.add(new Walker());
+        };      
+                
+        // GAME LOOP
+        while (currFloor <= NUM_FLOORS) {
+            // Set floor specifics
+            switch (currFloor) {
+                case 1: // Floor 1 - Just Walkers
+                    for (int i = 0; i < HORDE_SIZE; i++){
+                       zombieHorde.add(new Walker());
+                    }
+                    break;
+                case 2: // Floor 2 - Walkers + Tanks
+                    break;
+                case 3: // FLoor 3 - Walkers + Tanks + 1 Unknown
+                    break;
+                default:
+            }
+            
+            fight(zombieHorde);
+            System.out.println();
+            
+            currFloor++;
         }
-        
-        fight(zombieHorde);
-        System.out.println();
     }
 
     public static List survivorsGo(List<Zombie> zombiesAlive) {
@@ -69,37 +87,30 @@ public class Zombicide {
         return zombiesAlive;
     }
 
-    // public static void zombiesGo(List<Zombie> zombieAlive) {
-    //     int NUM_ZOMBIE_ATTACKS = 1;
-    //     int target; // Current attack target
+    public static void zombiesGo(List<Zombie> zombieAlive) {
+        int NUM_ZOMBIE_ATTACKS = 1;
+        int target; // Current attack target
 
-    //     /*
-    //     // To store alive survivors' index
-    //     List<Integer> survivorsAlive = new ArrayList<>();
-    //     for (int i = 0; i < myTeam.length; i++) {
-    //         survivorsAlive.add(i);
-    //     } */
+        System.out.println("-- ZombieHorde attacks --");
+        for (Zombie z : zombieAlive) {
+            for (int i = 0; i < NUM_ZOMBIE_ATTACKS; i++) {
+                // Get a random survivor's index
+                target =  (int)(Math.random() * myTeam.length);
 
-    //     System.out.println("-- ZombieHorde attacks --");
-    //     for (Zombie z : zombieHorde) {
-    //         for (int i = 0; i < NUM_ZOMBIE_ATTACKS; i++) {
-    //             // Get a random survivor's index
-    //             target =  (int)(Math.random() * myTeam.length);
+                // Make z hit target
+                if (z.hit()) {
+                    // TO DO: make z do damage
+                    myTeam[target].setHealth(myTeam[target].getHealth() - z.getDamage());
+                    System.out.println(myTeam[target].getName() + " was hit! HP remaining: " + myTeam[target].getHealth());
+                } else {
+                    System.out.println("Fail");
+                }
 
-    //             // Make z hit target
-    //             if (z.hit()) {
-    //                 // TO DO: make z do damage
-    //                 myTeam[target].setHealth(myTeam[target].getHealth() - z.getDamage());
-    //                 System.out.println(myTeam[target].getName() + " was hit! HP remaining: " + myTeam[target].getHealth());
-    //             } else {
-    //                 System.out.println("Fail");
-    //             }
-
-    //             // TO DO: Lose logic
-    //         }
-    //         System.out.println();
-    //     }
-    // }
+                // TO DO: Lose logic
+            }
+            System.out.println();
+        }
+    }
 
     public static void fight(List<Zombie> zombieHorde) {
         zombieHorde = survivorsGo(zombieHorde);
